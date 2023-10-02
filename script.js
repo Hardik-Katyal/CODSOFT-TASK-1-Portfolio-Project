117048fb9800273308fb15b4ae2644b4ef3aef85
Hardik-Katyal <hardikkatyalofficial@gmail.com>
2023-10-02 17:46:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".coco-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
